07e95111020b699085e5cac4c91e1af0ac6c02a7
wallace <wallace.nfesistemas@gmail.com>
2019-01-15 14:52:35 -0200

    Updates

---
+++ precache-manifest.a596f8797b1299f78c0ecb72f173f9e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deep-purple/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "555d61780de71b0c70cd",
+    "url": "/deep-purple/static/js/runtime~main.555d6178.js"
+  },
+  {
+    "revision": "e9c727a3dbfbceddcf47",
+    "url": "/deep-purple/static/js/main.e9c727a3.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/deep-purple/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e9c727a3dbfbceddcf47",
+    "url": "/deep-purple/static/css/main.739596ab.chunk.css"
+  },
+  {
+    "revision": "f588c3c07a12fe366ff64d9f475b3691",
+    "url": "/deep-purple/index.html"
+  }
+];
