7bcd304b701a2b26b3c921e79aa37913261d7f7f
wallace <wallace.nfesistemas@gmail.com>
2019-01-16 18:52:48 -0200

    added smooth scroll

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      teste: null,
-      logo: null,
-      main_picture: null
-    };
+    this.state = {}
   }
 
   fetchData() {
-    fetch(`./Band2.json`)
+    fetch(`./Band1.json`)
       .then(response => response.json())
       .then(data =>
         this.setState({
-          teste: 'testinho',
           logo: data.logo,
-          main_picture: data.main_picture
+          main_picture: data.main_picture,
+          band_name: data.band_name,
+          local: data.local,
+          date: data.date,
+          time: data.time,
+          sectors: data.sectors
         })
       )
       .catch(error => this.setState({ error, isLoading: false }));
@@ -32,7 +32,7 @@ class App extends Component {
   }
 
   render() {
-    const { teste, logo, main_picture} = this.state;
+    const {logo, main_picture, band_name, local, date, time, sectors} = this.state;
     return (
       <div className="App"> 
         <div className="App__background" >
@@ -43,9 +43,7 @@ class App extends Component {
             <div className="App__background__content__header" >
               <img src={main_picture} />
             </div>
-            <div>
-            </div>
-            <Description />
+            <Description local={local} date={date} time={time} sectors={sectors} />
           </div>
         </div>
       </div>
--- src/Description.js
+++ src/Description.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import './Description.scss';
 
-
 export class Description extends Component {
+	constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    document.getElementById('sectors').scrollIntoView({ 
+		  behavior: 'smooth' 
+		});
+  }
+
   render() {
     return (
       <div className="Description">
-      	<div className="Description__content">
-      		<a>Compre Agora!</a>
+      	<div className="Description__button">
+      		<a onClick={this.handleClick} >Compre Agora! </a>
+      	</div>
+      	<div className="Description__local">
+      		<div className="Description__local_name">{this.props.local}</div>
+      		<div className="Description__local_date">{this.props.date}</div>
+      		<div className="Description__local_time">{this.props.time}</div>
+      	</div>
+      	<div className="Description__sectors" id="sectors">
+      		<div className="Description__local_name">{this.props.local}</div>
+      		<div className="Description__local_date">{this.props.date}</div>
+      		<div className="Description__local_time">{this.props.time}</div>
       	</div>
       </div>
     );
