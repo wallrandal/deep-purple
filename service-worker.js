41c9152b77de722e11f7a46dc98e25b0ff31a068
wallace <wallace.nfesistemas@gmail.com>
2019-01-17 11:56:11 -0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/deep-purple/precache-manifest.93327b8ae4d36544aa9ba6a95197eb74.js"
+  "/deep-purple/precache-manifest.20e8fc44ac7e19ada040410fb7d6374c.js"
 );
 
 workbox.clientsClaim();
